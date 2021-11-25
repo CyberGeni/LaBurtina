2813cc2da88584dd53d8cc8a1cec71429147cf6b
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2021-11-25 16:51:05 +0100

    added a 'quote'

--- src/components/shop/product-item.vue
+++ src/components/shop/product-item.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Yoooooo. Stay tuned... expect something hoooge 😁<br> Keep your sass for CSS. Everywhere else, be kind.<br><br> *drops mic*</h1>
+        <h1>Yoooooo. Stay tuned... expect something hoooge 😁<br>one more thing... <br><br> Keep your sass for CSS. Everywhere else, be kind.<br><br> *drops mic*</h1>
     </div>
 </template>
 <script>
